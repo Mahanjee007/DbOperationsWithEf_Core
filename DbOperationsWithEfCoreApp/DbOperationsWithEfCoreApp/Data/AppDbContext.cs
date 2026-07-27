4810354c385315c7c6386e5bc4c52db971391576
MrKant007 <krishnak@owen-media.com>
2026-07-27 18:26:30 +0530

    initial code

---
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEfCoreApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
